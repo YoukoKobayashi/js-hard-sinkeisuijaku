ad7a4e78208b2278adc02ff562cab80ba06beb24
YoukoKobayashi <86542067+YoukoKobayashi@users.noreply.github.com>
2022-11-15 13:03:23 +0900

    Add files via upload
    チャレンジ問題終了

--- main.js
+++ main.js
@@ -17,7 +17,17 @@ for (let l = arrNum.length; l > 1; l--) {
 }
 console.log(arrNum); // 視覚化する
 
+const nextPlayer = document.getElementById("nextPlayer");
+const player1Point = document.getElementById("player1Point");
+const player2Point = document.getElementById("player2Point");
 let judgeNum = [];
+let cnt1 = 0;
+let cnt2 = 0;
+const tgt1 = "次はPlayer1の番です";
+const tgt2 = "次はPlayer2の番です";
+nextPlayer.textContent = "次はPlayer1の番です";
+player1Point.textContent = `player1:${cnt1}`;
+player2Point.textContent = `player2:${cnt2}`;
 for (let n = 0; n < arrNum.length; n++) {
   const div = document.createElement("div");
   div.setAttribute("class", "card back");
@@ -37,16 +47,35 @@ for (let n = 0; n < arrNum.length; n++) {
           //console.log("いっしょ");  挙動の確認
           judgeNum[0].className = "card finish";
           judgeNum[1].className = "card finish";
+          //challenge
+          if (nextPlayer.textContent === tgt1) {
+            nextPlayer.textContent = tgt1;
+            cnt1 += 1;
+          } else if (nextPlayer.textContent === tgt2) {
+            nextPlayer.textContent = tgt2;
+            cnt2 += 1;
+          }
         } else {
           //console.log("違う");  挙動の確認
           judgeNum[0].className = "card back";
           judgeNum[1].className = "card back";
           judgeNum[0].textContent = "";
           judgeNum[1].textContent = "";
+          //challenge
+          if (nextPlayer.textContent === tgt1) {
+            nextPlayer.textContent = tgt2;
+          } else if (nextPlayer.textContent === tgt2) {
+            nextPlayer.textContent = tgt1;
+          }
         }
         judgeNum = [];
+        player1Point.textContent = `Player1:${cnt1}`;
+        player2Point.textContent = `Player2:${cnt2}`;
+        if (cnt1 + cnt2 === 4) {
+          alert("終了です");
+        }
       }
-    }, 2000);
+    }, 4000);
   });
   panel.appendChild(div);
 }
